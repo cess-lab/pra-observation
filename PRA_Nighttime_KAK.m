ea44ef34bdc36cef4353cc7917b996c1e3a6f8be
syaifulafrizal <114385927+syaifulafrizal@users.noreply.github.com>
2025-04-29 22:05:27 +0800

    Update PRA_Nighttime_KAK.m
    Signed-off-by: syaifulafrizal <114385927+syaifulafrizal@users.noreply.github.com>

--- PRA_Nighttime_KAK.m
+++ PRA_Nighttime_KAK.m
@@ -114,7 +114,8 @@ else
     T = table('Size',[0 2], 'VariableTypes', ["datetime","double"], 'VariableNames', ["Date","Threshold"]);
 end
 
-T = [T; table(todayClean, todayThr)];
+newRow = table(todayClean, todayThr, 'VariableNames', T.Properties.VariableNames);
+T = [T; newRow];
 writetable(T, thresholdFile, 'Delimiter', '\t');
 
 % Weighted threshold
